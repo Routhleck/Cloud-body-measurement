9d74c053987667f89dc43784825c5292f2296788
jiangjie1111 <113899340+jiangjie1111@users.noreply.github.com>
2023-07-06 16:44:03 +0800

    完善前端界面

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import ElementPlus from 'element-plus'
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-import 'element-plus/dist/index.css'
-import '@/assets/css/gloabal.css'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementPlus from "element-plus";
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+import axios from "axios";
+import "element-plus/dist/index.css";
+import "@/assets/css/gloabal.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
-const app = createApp(App)
-app.use(store).use(router).use(ElementPlus, {
+const app = createApp(App);
+app
+  .use(store)
+  .use(router)
+  .use(ElementPlus, {
     locale: zhCn,
-  }).mount('#app')
+  })
+  .mount("#app").config.globalProperties.$axios = axios
+  .use(router)
+  .mount("#app");
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
+  app.component(key, component);
 }
--- frontend/src/views/SubmitView.vue
+++ frontend/src/views/SubmitView.vue
@@ -1,38 +1,73 @@
 <template>
-  <div>
-    <form @submit.prevent="submitForm">
-      <label for="userId">用户ID:</label>
-      <input type="text" id="userId" v-model="formData.userId" required><br>
-      <label for="testTime">体测时间:</label>
-      <input type="datetime-local" id="testTime" v-model="formData.testTime" required><br>
-      <label for="height">身高(cm):</label>
-      <input type="number" id="height" v-model="formData.height" required><br>
-      <label for="weight">体重(kg):</label>
-      <input type="number" id="weight" v-model="formData.weight" required><br>
-      <label for="vitalCapacity">肺活量(ml):</label>
-      <input type="number" id="vitalCapacity" v-model="formData.vitalCapacity" required><br>
-      <label for="standingLongJump">立定跳远(cm):</label>
-      <input type="number" id="standingLongJump" v-model="formData.standingLongJump" required><br>
-      <label for="sitAndReach">坐位体前屈(cm):</label>
-      <input type="number" id="sitAndReach" v-model="formData.sitAndReach" required><br>
-      <label for="pullOrSitUp">引体向上/仰卧起坐(time):</label>
-      <input type="number" id="pullOrSitUp" v-model="formData.pullOrSitUp" required><br>
-      <label for="fiftyM">50米跑(second):</label>
-      <input type="number" id="fiftyM" v-model="formData.fiftyM" required><br>
-      <label for="eightHundredOrThousandM">800或1000米(second):</label>
-      <input type="number" id="eightHundredOrThousandM" v-model="formData.eightHundredOrThousandM" required><br>
-      <button type="submit">提交</button>
-    </form>
+  <div class="form-container">
+    <div class="form">
+      <el-form ref="form" label-width="200px" label-position="left">
+        <div class="form-item">
+          <el-form-item label="用户ID" required>
+            <el-input v-model="formData.userId"></el-input>
+          </el-form-item>
+        </div>
+        <div class="form-item">
+          <el-form-item label="身高(cm)">
+            <el-input-number v-model="formData.height"></el-input-number>
+          </el-form-item>
+        </div>
+        <div class="form-item">
+          <el-form-item label="体重(kg)">
+            <el-input-number v-model="formData.weight"></el-input-number>
+          </el-form-item>
+        </div>
+        <div class="form-item">
+          <el-form-item label="肺活量(ml)">
+            <el-input-number v-model="formData.vitalCapacity"></el-input-number>
+          </el-form-item>
+        </div>
+        <div class="form-item">
+          <el-form-item label="立定跳远(cm)">
+            <el-input-number
+              v-model="formData.standingLongJump"
+            ></el-input-number>
+          </el-form-item>
+        </div>
+        <div class="form-item">
+          <el-form-item label="坐位体前屈(cm)">
+            <el-input-number v-model="formData.sitAndReach"></el-input-number>
+          </el-form-item>
+        </div>
+        <div class="form-item">
+          <el-form-item label="引体向上/仰卧起坐(time)">
+            <el-input-number v-model="formData.pullOrSitUp"></el-input-number>
+          </el-form-item>
+        </div>
+        <div class="form-item">
+          <el-form-item label="50米跑(second)">
+            <el-input-number v-model="formData.fiftyM"></el-input-number>
+          </el-form-item>
+        </div>
+        <div class="form-item">
+          <el-form-item label="800或1000米(second)">
+            <el-input-number
+              v-model="formData.eightHundredOrThousandM"
+            ></el-input-number>
+          </el-form-item>
+        </div>
+        <div class="form-item">
+          <el-form-item>
+            <el-button type="primary" native-type="submit">提交</el-button>
+          </el-form-item>
+        </div>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       formData: {
-        userId: '',
-        testTime: '',
+        userId: "",
         height: null,
         weight: null,
         vitalCapacity: null,
@@ -40,16 +75,62 @@ export default {
         sitAndReach: null,
         pullOrSitUp: null,
         fiftyM: null,
-        eightHundredOrThousandM: null
-      }
+        eightHundredOrThousandM: null,
+      },
     };
   },
   methods: {
     submitForm() {
       // 将数据提交到服务器端
       // 使用formData对象中的数据发送POST请求到后端API，并将数据存储到数据库中
-      // 在这里编写你的HTTP请求逻辑，使用Axios、Fetch API等库发送请求
-    }
-  }
+
+      // 构造要提交的数据对象
+      const data = {
+        user_id: this.formData.userId,
+        test_time: this.formData.testTime,
+        height: this.formData.height,
+        weight: this.formData.weight,
+        vital_capacity: this.formData.vitalCapacity,
+        standing_long_jump: this.formData.standingLongJump,
+        sit_and_reach: this.formData.sitAndReach,
+        pull_or_sit_up: this.formData.pullOrSitUp,
+        "50m": this.formData.fiftyM,
+        "800_or_1000m": this.formData.eightHundredOrThousandM,
+      };
+
+      // 发送POST请求到后端API
+      axios
+        .post("/api/submitTest", data)
+        // eslint-disable-next-line
+        .then((response) => {
+          // 请求成功处理逻辑
+          console.log("数据提交成功");
+          // 在这里可以根据API返回的响应做出相应的处理
+        })
+        .catch((error) => {
+          // 请求失败处理逻辑
+          console.error("数据提交失败:", error);
+          // 在这里可以根据错误进行适当的处理，例如显示错误消息
+        });
+    },
+  },
 };
 </script>
+
+<style scoped>
+.form-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+  height: 100vh;
+}
+
+.form {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
