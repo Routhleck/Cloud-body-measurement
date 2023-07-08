76c72352228c17a7a893d3fc8d85042e45c65764
Routhleck <1310722434@qq.com>
2023-07-08 15:26:11 +0800

    Rename variable and function

--- android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/algorithm/PoseEmbedder.kt
+++ android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/algorithm/PoseEmbedder.kt
@@ -4,9 +4,9 @@ import com.google.mediapipe.examples.poselandmarker.algorithm.Util.MyArray
 import kotlin.math.max
 
 class PoseEmbedder(torsoSizeMultiplier: Double = 2.5) {
-    private val _torso_size_multiplier = torsoSizeMultiplier
+    private val _torsoSizeMultiplier = torsoSizeMultiplier
 
-    private val _landmark_names = listOf(
+    private val _landmarkNames = listOf(
         "nose",
         "left_eye_inner", "left_eye", "left_eye_outer",
         "right_eye_inner", "right_eye", "right_eye_outer",
@@ -26,19 +26,19 @@ class PoseEmbedder(torsoSizeMultiplier: Double = 2.5) {
     )
 
 
-    private fun _get_pose_center(landmarks: Array<DoubleArray>): DoubleArray {
+    private fun getPoseCenter(landmarks: Array<DoubleArray>): DoubleArray {
         /*
         Calculates pose center as point between hips.将姿势中心计算为臀部之间的点。"""
         left_hip = landmarks[self._landmark_names.index('left_hip')]
         right_hip = landmarks[self._landmark_names.index('right_hip')]
         center = (left_hip + right_hip) * 0.5
         */
-        val left_thumb_2 = landmarks[_landmark_names.indexOf("left_thumb_2")]
-        val right_thumb_2 = landmarks[_landmark_names.indexOf("right_thumb_2")]
-        return ArrayOperation.averageDoubleArray(left_thumb_2, right_thumb_2)
+        val leftThumb2 = landmarks[_landmarkNames.indexOf("left_thumb_2")]
+        val rightThumb2 = landmarks[_landmarkNames.indexOf("right_thumb_2")]
+        return ArrayOperation.averageDoubleArray(leftThumb2, rightThumb2)
     }
 
-    private fun _get_pose_size(landmarks: Array<DoubleArray>, torso_size_multiplier: Double): Double {
+    private fun getPoseSize(landmarks: Array<DoubleArray>, torsoSizeMultiplier: Double): Double {
         /*
         计算姿势大小。
         它是下面两个值的最大值:
@@ -49,27 +49,27 @@ class PoseEmbedder(torsoSizeMultiplier: Double = 2.5) {
         val landmarks2d = ArrayOperation.getLandmarksByDimensionRange(landmarks, 0..1)
 
         // 两手中心
-        val left_hand = landmarks[_landmark_names.indexOf("left_wrist")]
-        val right_hand = landmarks[_landmark_names.indexOf("right_wrist")]
-        val hands = ArrayOperation.averageDoubleArray(left_hand, right_hand)
+        val leftHand = landmarks[_landmarkNames.indexOf("left_wrist")]
+        val rightHand = landmarks[_landmarkNames.indexOf("right_wrist")]
+        val hands = ArrayOperation.averageDoubleArray(leftHand, rightHand)
 
         // 两臀中心
-        val left_hip = landmarks2d[_landmark_names.indexOf("left_hip")]
-        val right_hip = landmarks2d[_landmark_names.indexOf("right_hip")]
-        val hips = ArrayOperation.averageDoubleArray(left_hip, right_hip)
+        val leftHip = landmarks2d[_landmarkNames.indexOf("left_hip")]
+        val rightHip = landmarks2d[_landmarkNames.indexOf("right_hip")]
+        val hips = ArrayOperation.averageDoubleArray(leftHip, rightHip)
 
         // 躯干尺寸作为最小的身体尺寸
-        val torso_size = ArrayOperation.getNorm(MyArray(hands).sub(MyArray(hips)))
+        val torsoSize = ArrayOperation.getNorm(MyArray(hands).sub(MyArray(hips)))
 
         // 到姿势中心的最大距离
-        val pose_center = _get_pose_center(landmarks)
-        val diff = landmarks2d.map { ArrayOperation.getNorm(MyArray(it).sub(MyArray(pose_center))) }
-        val max_dist = diff.maxOrNull() ?: 0.0
+        val poseCenter = getPoseCenter(landmarks)
+        val diff = landmarks2d.map { ArrayOperation.getNorm(MyArray(it).sub(MyArray(poseCenter))) }
+        val maxDist = diff.maxOrNull() ?: 0.0
 
-        return max(torso_size, max_dist)
+        return max((torsoSize * torsoSizeMultiplier), maxDist)
     }
 
-    private fun _normalize_pose_landmarks(landmarks: Array<DoubleArray>): Array<DoubleArray> {
+    private fun normalizePoseLandmarks(landmarks: Array<DoubleArray>): Array<DoubleArray> {
         /*
         将姿势地标归一化为单位尺寸。
         1. 将姿势中心移动到原点。
@@ -79,12 +79,12 @@ class PoseEmbedder(torsoSizeMultiplier: Double = 2.5) {
         var landmarks3d = landmarks
 
         // 1. 将姿势中心移动到原点。
-        val pose_center = _get_pose_center(landmarks)
-        landmarks3d = ArrayOperation.subArrayDoubleArrayByDoubleArray(landmarks3d, pose_center)
+        val poseCenter = getPoseCenter(landmarks)
+        landmarks3d = ArrayOperation.subArrayDoubleArrayByDoubleArray(landmarks3d, poseCenter)
 
         // 2. 将姿势缩放到单位大小。
-        val pose_size = _get_pose_size(landmarks, _torso_size_multiplier)
-        landmarks3d = ArrayOperation.divArrayDoubleArrayByDouble(landmarks3d, pose_size)
+        val poseSize = getPoseSize(landmarks, _torsoSizeMultiplier)
+        landmarks3d = ArrayOperation.divArrayDoubleArrayByDouble(landmarks3d, poseSize)
 
         // Multiplication by 100 is not required, but makes it eaasier to debug
         landmarks3d = ArrayOperation.multiplyArrayDoubleArrayByDouble(landmarks3d, 100.0)
@@ -92,7 +92,7 @@ class PoseEmbedder(torsoSizeMultiplier: Double = 2.5) {
         return landmarks3d
     }
 
-    private fun _get_pose_distance_embedding(landmarks: Array<DoubleArray>): DoubleArray {
+    private fun getPoseDistanceEmbedding(landmarks: Array<DoubleArray>): DoubleArray {
         /*
         将姿势landmarks转换为 3D embedding.
         我们使用几个成对的 3D 距离来形成姿势embedding。 所有距离都包括带符号的 X 和 Y 分量。
@@ -106,85 +106,85 @@ class PoseEmbedder(torsoSizeMultiplier: Double = 2.5) {
         val embeddingList: MutableList<Double> = mutableListOf()
 
         // one joint.
-        val jointDistance = _get_distance(
-            _get_average_by_names(landmarks, "left_hip", "right_hip"),
-            _get_average_by_names(landmarks, "left_thumb_2", "right_thumb_2")
+        val jointDistance = getDistance(
+            getAverageByNames(landmarks, "left_hip", "right_hip"),
+            getAverageByNames(landmarks, "left_thumb_2", "right_thumb_2")
         )
         embeddingList.add(jointDistance)
 
         // 左眼与左肩的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_eye", "left_shoulder"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_eye", "left_shoulder"))
         // 右眼与右肩的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "right_eye", "right_shoulder"))
+        embeddingList.add(getDistanceByNames(landmarks, "right_eye", "right_shoulder"))
         // 左眼与左手的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_eye", "left_thumb_2"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_eye", "left_thumb_2"))
         // 右眼与右手的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "right_eye", "right_thumb_2"))
+        embeddingList.add(getDistanceByNames(landmarks, "right_eye", "right_thumb_2"))
         // 鼻子与左肩的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "nose", "left_shoulder"))
+        embeddingList.add(getDistanceByNames(landmarks, "nose", "left_shoulder"))
         // 鼻子与右肩的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "nose", "right_shoulder"))
+        embeddingList.add(getDistanceByNames(landmarks, "nose", "right_shoulder"))
         // 鼻子与左手的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "nose", "left_thumb_2"))
+        embeddingList.add(getDistanceByNames(landmarks, "nose", "left_thumb_2"))
         // 鼻子与右手的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "nose", "right_thumb_2"))
+        embeddingList.add(getDistanceByNames(landmarks, "nose", "right_thumb_2"))
         // 左嘴与左肩的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "mouth_left", "left_shoulder"))
+        embeddingList.add(getDistanceByNames(landmarks, "mouth_left", "left_shoulder"))
         // 右嘴与右肩的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "mouth_right", "right_shoulder"))
+        embeddingList.add(getDistanceByNames(landmarks, "mouth_right", "right_shoulder"))
         // 左嘴与左手的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "mouth_left", "left_thumb_2"))
+        embeddingList.add(getDistanceByNames(landmarks, "mouth_left", "left_thumb_2"))
         // 右嘴与右手的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "mouth_right", "right_thumb_2"))
+        embeddingList.add(getDistanceByNames(landmarks, "mouth_right", "right_thumb_2"))
         // 左肩膀与左肘的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_shoulder", "left_elbow"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_shoulder", "left_elbow"))
         // 右肩膀与右肘的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "right_shoulder", "right_elbow"))
+        embeddingList.add(getDistanceByNames(landmarks, "right_shoulder", "right_elbow"))
         // 左肘和左手腕的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_elbow", "left_wrist"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_elbow", "left_wrist"))
         // 右肘和右手腕的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "right_elbow", "right_wrist"))
+        embeddingList.add(getDistanceByNames(landmarks, "right_elbow", "right_wrist"))
         // 左髋关节与左膝盖的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_hip", "left_knee"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_hip", "left_knee"))
         // 右髋关节与右膝盖的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "right_hip", "right_knee"))
+        embeddingList.add(getDistanceByNames(landmarks, "right_hip", "right_knee"))
         // 左膝盖和左踝的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_knee", "left_ankle"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_knee", "left_ankle"))
         // 右膝盖和右踝的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "right_knee", "right_ankle"))
+        embeddingList.add(getDistanceByNames(landmarks, "right_knee", "right_ankle"))
         // 左肩膀与左手腕的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_shoulder", "left_wrist"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_shoulder", "left_wrist"))
         // 右肩膀与右手腕的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "right_shoulder", "right_wrist"))
+        embeddingList.add(getDistanceByNames(landmarks, "right_shoulder", "right_wrist"))
         // 左髋关节与左踝的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_hip", "left_ankle"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_hip", "left_ankle"))
         // 右髋关节与右踝的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "right_hip", "right_ankle"))
+        embeddingList.add(getDistanceByNames(landmarks, "right_hip", "right_ankle"))
         // 左髋关节与左肘的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_hip", "left_elbow"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_hip", "left_elbow"))
         // 右髋关节与右肘的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "right_hip", "right_elbow"))
+        embeddingList.add(getDistanceByNames(landmarks, "right_hip", "right_elbow"))
         // 左肩与左脚踝的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_shoulder", "left_ankle"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_shoulder", "left_ankle"))
         // 右肩与右脚踝的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "right_shoulder", "right_ankle"))
+        embeddingList.add(getDistanceByNames(landmarks, "right_shoulder", "right_ankle"))
         // 左髋关节与左手腕的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_hip", "left_wrist"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_hip", "left_wrist"))
         // 右髋关节与右手腕的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "right_hip", "right_wrist"))
+        embeddingList.add(getDistanceByNames(landmarks, "right_hip", "right_wrist"))
         // 左肘与右肘的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_elbow", "right_elbow"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_elbow", "right_elbow"))
         // 左膝盖与右膝盖的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_knee", "right_knee"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_knee", "right_knee"))
         // 左脚踝与右脚踝的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_ankle", "right_ankle"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_ankle", "right_ankle"))
         // 左手腕与右手腕的距离。
-        embeddingList.add(_get_distance_by_names(landmarks, "left_wrist", "right_wrist"))
+        embeddingList.add(getDistanceByNames(landmarks, "left_wrist", "right_wrist"))
 
         return ArrayOperation.vstack(embeddingList)
     }
 
-    private fun _get_average_by_names(landmarks: Array<DoubleArray>, name_from: String, name_to: String): DoubleArray {
+    private fun getAverageByNames(landmarks: Array<DoubleArray>, name_from: String, name_to: String): DoubleArray {
         /*
         计算两个姿势地标之间的平均值。
         Args:
@@ -197,13 +197,13 @@ class PoseEmbedder(torsoSizeMultiplier: Double = 2.5) {
         */
         val landmarks2d = ArrayOperation.getLandmarksByDimensionRange(landmarks, 0..1)
 
-        val lmk_from = landmarks2d[_landmark_names.indexOf(name_from)]
-        val lmk_to = landmarks2d[_landmark_names.indexOf(name_to)]
+        val lmkFrom = landmarks2d[_landmarkNames.indexOf(name_from)]
+        val lmkTo = landmarks2d[_landmarkNames.indexOf(name_to)]
 
-        return ArrayOperation.averageDoubleArray(lmk_from, lmk_to)
+        return ArrayOperation.averageDoubleArray(lmkFrom, lmkTo)
     }
 
-    private fun _get_distance_by_names(landmarks: Array<DoubleArray>, name_from: String, name_to: String): Double {
+    private fun getDistanceByNames(landmarks: Array<DoubleArray>, name_from: String, name_to: String): Double {
         /*
         计算两个姿势地标之间的距离。
         Args:
@@ -216,13 +216,13 @@ class PoseEmbedder(torsoSizeMultiplier: Double = 2.5) {
         */
         val landmarks2d = ArrayOperation.getLandmarksByDimensionRange(landmarks, 0..1)
 
-        val lmk_from = landmarks2d[_landmark_names.indexOf(name_from)]
-        val lmk_to = landmarks2d[_landmark_names.indexOf(name_to)]
+        val lmkFrom = landmarks2d[_landmarkNames.indexOf(name_from)]
+        val lmkTo = landmarks2d[_landmarkNames.indexOf(name_to)]
 
-        return _get_distance(lmk_from, lmk_to)
+        return getDistance(lmkFrom, lmkTo)
     }
 
-    private fun _get_distance(lmk_from: DoubleArray, lmk_to: DoubleArray): Double {
+    private fun getDistance(lmk_from: DoubleArray, lmk_to: DoubleArray): Double {
         /*
         计算两个3D点之间的距离。
         */
@@ -241,13 +241,13 @@ class PoseEmbedder(torsoSizeMultiplier: Double = 2.5) {
           pairwise distances defined in `_get_pose_distance_embedding`.
           具有形状 (M, 3) 的姿势embedding的 Numpy 数组，其中“M”是“_get_pose_distance_embedding”中定义的成对距离的数量。
         */
-        assert(landmarks.size == _landmark_names.size) { "Expected ${_landmark_names.size} landmarks, but got ${landmarks.size}" }
+        assert(landmarks.size == _landmarkNames.size) { "Expected ${_landmarkNames.size} landmarks, but got ${landmarks.size}" }
 
         // Normalize landmarks.
-        var newLandmarks = _normalize_pose_landmarks(landmarks)
+        val newLandmarks = normalizePoseLandmarks(landmarks)
 
         // Get embedding.
-        return _get_pose_distance_embedding(newLandmarks)
+        return getPoseDistanceEmbedding(newLandmarks)
 
     }
 
