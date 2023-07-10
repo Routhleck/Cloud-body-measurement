b55a6ddc6c4a07fa8c7ad4b6b913301905fd9f02
jiangjie1111 <113899340+jiangjie1111@users.noreply.github.com>
2023-07-10 17:19:16 +0800

    完善身份认证
    Co-Authored-By: He Yi <85550446+heyi755@users.noreply.github.com>

--- frontend/src/views/AuthenticationView.vue
+++ frontend/src/views/AuthenticationView.vue
@@ -1,199 +1,194 @@
 <template>
   <div class="camera_outer">
-    <video
-      id="videoCamera"
-      :width="videoWidth"
-      :height="videoHeight"
-      autoplay
-    ></video>
-    <canvas
-      style="display: none"
-      id="canvasCamera"
-      :width="videoWidth"
-      :height="videoHeight"
-    ></canvas>
-
-    <div v-if="imgSrc" class="img_bg_camera">
-      <p>效果预览</p>
-      <img :src="imgSrc" alt class="tx_img" />
-    </div>
-
-    <div class="button_container">
-      <div class="button">
-        <el-button @click="getCompetence()">打开摄像头</el-button>
-        <el-button @click="stopNavigator()">关闭摄像头</el-button>
-        <el-button @click="setImage()">拍照</el-button>
+    <div class="left_section">
+      <h2>上传身份证照片</h2>
+      <div class="upload_button_container">
+        <input
+          type="file"
+          accept="image/*"
+          @change="selectIdCardImage"
+          ref="idCardInput"
+          style="display: none"
+        />
+        <button @click="openIdCardInput" class="upload_button">
+          选择身份证照片
+        </button>
+        <button @click="uploadIdCard" class="upload_button">
+          上传身份证照片
+        </button>
       </div>
-
-      <div>
-        <el-button @click="openUploadDialog" class="upload">上传照片</el-button>
+      <div v-if="idCardImage" class="img_preview">
+        <img :src="idCardImage" alt="身份证照片预览" />
       </div>
     </div>
-
-    <el-dialog title="确认上传" v-model="dialogVisible" width="30%">
-      <template #default>
-        <span>确定要上传照片吗？</span>
-      </template>
-      <template #footer>
-        <el-button @click="uploadPhoto">确定</el-button>
-        <el-button @click="cancelUpload">取消</el-button>
-      </template>
-    </el-dialog>
+    <div class="right_section">
+      <h2>拍摄照片</h2>
+      <video
+        id="videoCamera"
+        :width="videoWidth"
+        :height="videoHeight"
+        autoplay
+      ></video>
+      <div class="upload_button_container">
+        <button @click="getCompetence" class="upload_button">打开摄像头</button>
+        <button @click="captureImage" class="upload_button">拍照</button>
+        <button @click="uploadPhoto" class="upload_button">上传照片</button>
+      </div>
+      <div v-if="cameraImage" class="img_preview">
+        <img :src="cameraImage" alt="拍摄照片预览" />
+      </div>
+    </div>
+    <div class="submit_section">
+      <button
+        @click="submitAuthentication"
+        class="submit_button"
+        :disabled="!idCardImage || !cameraImage"
+      >
+        提交认证
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 
-function dataURLtoFile(dataURL, filename) {
-  const arr = dataURL.split(",");
-  const mime = arr[0].match(/:(.*?);/)[1];
-  const bstr = atob(arr[1]);
-  let n = bstr.length;
-  const u8arr = new Uint8Array(n);
-  while (n--) {
-    u8arr[n] = bstr.charCodeAt(n);
-  }
-  return new File([u8arr], filename, { type: mime });
-}
-
 export default {
   data() {
     return {
       videoWidth: 250,
       videoHeight: 350,
-      imgSrc: "",
-      thisCancas: null,
-      thisContext: null,
-      thisVideo: null,
-      openVideo: false,
-      dialogVisible: false,
-      capturedImageData: null,
+      idCardImage: null,
+      cameraImage: null,
+      idCardImageData: null,
+      cameraImageData: null,
     };
-  },
-  mounted() {
-    // this.getCompetence()//进入页面就调用摄像头
   },
   methods: {
     // 调用权限（打开摄像头功能）
     getCompetence() {
-      var _this = this;
-      _this.thisCancas = document.getElementById("canvasCamera");
-      _this.thisContext = this.thisCancas.getContext("2d");
-      _this.thisVideo = document.getElementById("videoCamera");
-      _this.thisVideo.style.display = "block";
-      // 获取媒体属性，旧版本浏览器可能不支持mediaDevices，我们首先设置一个空对象
-      if (navigator.mediaDevices === undefined) {
-        navigator.mediaDevices = {};
-      }
-      // 一些浏览器实现了部分mediaDevices，我们不能只分配一个对象
-      // 使用getUserMedia，因为它会覆盖现有的属性。
-      // 这里，如果缺少getUserMedia属性，就添加它。
-      if (navigator.mediaDevices.getUserMedia === undefined) {
-        navigator.mediaDevices.getUserMedia = function (constraints) {
-          // 首先获取现存的getUserMedia(如果存在)
-          var getUserMedia =
-            navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia ||
-            navigator.getUserMedia;
-          // 有些浏览器不支持，会返回错误信息
-          // 保持接口一致
-          if (!getUserMedia) {
-            // 不存在则报错
-            return Promise.reject(
-              new Error("getUserMedia is not implemented in this browser")
-            );
-          }
-          // 否则，使用Promise将调用包装到旧的navigator.getUserMedia
-          return new Promise(function (resolve, reject) {
-            getUserMedia.call(navigator, constraints, resolve, reject);
-          });
-        };
-      }
-      var constraints = {
-        audio: false,
-        video: {
-          width: this.videoWidth,
-          height: this.videoHeight,
-          transform: "scaleX(-1)",
-        },
-      };
+      const video = document.getElementById("videoCamera");
+
+      // 检查浏览器兼容性
       navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function (stream) {
-          // 旧的浏览器可能没有srcObject
-          if ("srcObject" in _this.thisVideo) {
-            _this.thisVideo.srcObject = stream;
-          } else {
-            // 避免在新的浏览器中使用它，因为它正在被弃用。
-            _this.thisVideo.src = window.URL.createObjectURL(stream);
-          }
-          _this.thisVideo.onloadedmetadata = function () {
-            _this.thisVideo.play();
-          };
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          video.srcObject = stream;
+          video.play();
         })
-        .catch((err) => {
-          console.log(err);
+        .catch((error) => {
+          console.error("无法打开摄像头：", error);
         });
     },
-    //  绘制图片（拍照功能）
-    setImage() {
-      var _this = this;
-      // canvas画图
-      _this.thisContext.drawImage(
-        _this.thisVideo,
-        0,
-        0,
-        _this.videoWidth,
-        _this.videoHeight
-      );
-      // 获取图片base64链接
-      var image = this.thisCancas.toDataURL("image/png");
-      _this.imgSrc = image; //赋值并预览图片
-      _this.capturedImageData = dataURLtoFile(image, "captured_image.png"); // 转换为File对象并保存图像数据
+    // 绘制图片（拍照功能）
+    captureImage() {
+      const video = document.getElementById("videoCamera");
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+      canvas.width = this.videoWidth;
+      canvas.height = this.videoHeight;
+
+      // 绘制视频画面到canvas
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+      // 获取canvas上的图像数据并转换为base64
+      const image = canvas.toDataURL("image/png");
+      this.cameraImage = image; // 赋值并预览图片
+      this.cameraImageData = this.dataURLtoFile(image, "camera_image.png"); // 转换为File对象并保存图像数据
     },
-    // 关闭摄像头
-    stopNavigator() {
-      this.thisVideo.srcObject.getTracks()[0].stop();
+    // 从本地选择身份证照片
+    selectIdCardImage(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.idCardImage = reader.result; // 赋值并预览图片
+          this.idCardImageData = file; // 保存图像数据
+        };
+        reader.readAsDataURL(file);
+      }
     },
-    // 打开上传照片对话框
-    openUploadDialog() {
-      this.dialogVisible = true;
+    // 打开文件选择器（选择身份证照片）
+    openIdCardInput() {
+      this.$refs.idCardInput.click();
     },
-    // 取消上传照片
-    cancelUpload() {
-      this.dialogVisible = false;
-    },
-    // 上传照片
-    uploadPhoto() {
-      // 获取图像数据
-      const imageData = this.capturedImageData;
-      const userJson = sessionStorage.getItem("user");
-      const user = JSON.parse(userJson);
-      const userId = user.user_id;
+    // 上传身份证照片
+    uploadIdCard() {
+      // 获取身份证照片数据
+      const imageData = this.idCardImageData;
 
       // 构建FormData对象
       const formData = new FormData();
       formData.append("file", imageData);
-      formData.append("userId", userId); // 替换为实际的用户ID
-      formData.append("prefix", "images"); // 替换为实际的文件前缀
 
-      // 发送POST请求
+      // 发送POST请求到身份证照片上传的后端API
       axios
-        .post("/upload", formData)
+        .post("http://your-backend-api-url/upload/idcard", formData)
         .then((response) => {
           // 上传成功的处理逻辑
-          console.log("照片上传成功！");
+          console.log("身份证照片上传成功！");
           console.log(response);
         })
         .catch((error) => {
           // 上传失败的处理逻辑
-          console.error("照片上传失败！", error);
-        })
-        .finally(() => {
-          // 上传完成后，关闭对话框
-          this.dialogVisible = false;
+          console.error("身份证照片上传失败！", error);
         });
+    },
+    // 上传拍摄的照片
+    uploadPhoto() {
+      // 获取拍摄照片数据
+      const imageData = this.cameraImageData;
+
+      // 构建FormData对象
+      const formData = new FormData();
+      formData.append("file", imageData);
+
+      // 发送POST请求到拍摄照片上传的后端API
+      axios
+        .post("http://your-backend-api-url/upload/camera", formData)
+        .then((response) => {
+          // 上传成功的处理逻辑
+          console.log("拍摄照片上传成功！");
+          console.log(response);
+        })
+        .catch((error) => {
+          // 上传失败的处理逻辑
+          console.error("拍摄照片上传失败！", error);
+        });
+    },
+    // 提交认证
+    submitAuthentication() {
+      // 判断两个照片是否都已上传，如果上传则调用认证API
+      if (this.idCardImageData && this.cameraImageData) {
+        // 调用认证API，传递身份证照片和拍摄照片数据
+        const formData = new FormData();
+        formData.append("idCardImage", this.idCardImageData);
+        formData.append("cameraImage", this.cameraImageData);
+
+        axios
+          .post("http://your-backend-api-url/authentication", formData)
+          .then((response) => {
+            // 认证成功的处理逻辑
+            console.log("认证成功！");
+            console.log(response);
+          })
+          .catch((error) => {
+            // 认证失败的处理逻辑
+            console.error("认证失败！", error);
+          });
+      }
+    },
+    // 将base64数据转换为File对象
+    dataURLtoFile(dataURL, filename) {
+      const arr = dataURL.split(",");
+      const mime = arr[0].match(/:(.*?);/)[1];
+      const bstr = atob(arr[1]);
+      let n = bstr.length;
+      const u8arr = new Uint8Array(n);
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+      }
+      return new File([u8arr], filename, { type: mime });
     },
   },
 };
@@ -201,24 +196,46 @@ export default {
 
 <style scoped>
 .camera_outer {
-  width: 100%;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
 }
-.button_container {
-  max-width: 100%;
-  margin: 50px;
-  padding: 50px;
+
+.left_section,
+.right_section {
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
 }
-.upload {
-  margin: 20px;
-  padding: 5px;
+
+.upload_button_container {
+  display: flex;
+  gap: 10px;
+  margin-top: 20px;
+}
+
+.img_preview {
+  margin-top: 20px;
+}
+
+.submit_section {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+}
+
+.submit_button {
+  padding: 10px 20px;
+  background-color: #4caf50;
+  color: #fff;
+  font-weight: bold;
+  border: none;
+  cursor: pointer;
+}
+
+.submit_button:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
 }
 </style>
