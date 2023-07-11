16c57133521ae4ca8e462cf469c57c9130fa1239
jiangjie1111 <113899340+jiangjie1111@users.noreply.github.com>
2023-07-11 14:29:26 +0800

    身份验证后端接收

--- backend/backend/src/main/java/com/sepbf/backend/controller/AuthController.java
+++ backend/backend/src/main/java/com/sepbf/backend/controller/AuthController.java
@@ -5,22 +5,29 @@ import okhttp3.RequestBody;
 import org.springframework.web.bind.annotation.*;
 import okhttp3.*;
 import org.json.JSONObject;
+
+import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
     public static final String API_KEY = FaceSecretKey.API_KEY;
     public static final String SECRET_KEY = FaceSecretKey.SECRET_KEY;
     public static final OkHttpClient HTTP_CLIENT = new OkHttpClient().newBuilder().build();
 
     @PostMapping
-    public String auth(@RequestParam("userId") Integer UserId) throws IOException {
-        int userId = UserId;
+    public String auth(@org.springframework.web.bind.annotation.RequestBody Map<String, Object> map) throws IOException {
+        System.out.println(map);
+        String userId = String.valueOf(map.get("userId"));
         System.out.println(userId);
         String userImagedir= "https://tcloud-1318685426.cos.ap-beijing.myqcloud.com/action%2FuserImages%2F" + userId + ".png";
         String checkImagedir= "https://tcloud-1318685426.cos.ap-beijing.myqcloud.com/action%2FcheckImages%2F" + userId + ".png";
+        System.out.println(userImagedir);
+        System.out.println(checkImagedir);
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody body = RequestBody.create(mediaType, String.format(
                 "[{\"image\":\"%s\",\"image_type\":\"URL\"},{\"image\":\"%s\",\"image_type\":\"URL\"}]",
