00d5b669160db12005a0a917358ca3fc6b788c4f
jiangjie1111 <113899340+jiangjie1111@users.noreply.github.com>
2023-07-12 16:46:11 +0800

    完善体测界面，添加训练界面

--- frontend/src/views/TestView.vue
+++ frontend/src/views/TestView.vue
@@ -1,33 +1,89 @@
 <template>
-  <div class="video_container">
-    <video ref="videoElement" controls></video>
+  <div class="element-main">
+    <el-table style="width: 100%" :data="tableData">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
+
+    <el-table style="width: 100%" :data="getValues()" :show-header="false">
+      <el-table-column
+        v-for="(item, index) in getHeaders()"
+        :key="index"
+        :prop="item"
+      >
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-import flvjs from 'flv.js'
-
 export default {
-  mounted () {
-    if (flvjs.isSupported()) {
-      let videoElement = this.$refs.videoElement
-      let flvPlayer = flvjs.createPlayer({
-        type: 'flv',
-        url: 'http://39.106.13.47:8080/live/111.live.flv'
-      })
-      flvPlayer.attachMediaElement(videoElement)
-      flvPlayer.load()
-      flvPlayer.play()
-    }
-  }
-}
+  data() {
+    return {
+      headers: [
+        {
+          prop: "date",
+          label: "日期",
+        },
+        {
+          prop: "name",
+          label: "姓名",
+        },
+        {
+          prop: "address",
+          label: "地址",
+        },
+      ],
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+  computed: {
+    getHeaders() {
+      //["title", "value0", "value1", "value2", "value3"]
+      return this.tableData.reduce(
+        (pre, cur, index) => pre.concat(`value${index}`),
+        ["title"]
+      );
+    },
+    getValues() {
+      //[ {title: "日期", value0: "2016-05-02", value1: "2016-05-04", value2: "2016-05-01", value3: "2016-05-03"} ]
+      return this.headers.map((item) => {
+        return this.tableData.reduce(
+          (pre, cur, index) =>
+            Object.assign(pre, { ["value" + index]: cur[item.prop] }),
+          { title: item.label }
+        );
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-.video_container{
+.element-main {
   max-width: 100%;
-  margin: 0 auto;
-  padding: 200px;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
