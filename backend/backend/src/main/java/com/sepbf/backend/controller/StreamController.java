8656cb9078308f6ee9ae1dbe24184d1578e8a8d1
陈增耀 <2692830800@qq.com>
2023-07-12 10:46:14 +0800

    springboot与flask通信，经过限制时间后返回count_value

--- backend/backend/src/main/java/com/sepbf/backend/controller/StreamController.java
+++ backend/backend/src/main/java/com/sepbf/backend/controller/StreamController.java
@@ -1,25 +1,32 @@
 package com.sepbf.backend.controller;
 
-import okhttp3.RequestBody;
-import org.springframework.web.bind.annotation.*;
-import okhttp3.*;
+import com.sepbf.backend.pojo.domain.toFlask;
 import com.sepbf.backend.service.ActionService;
+import com.sepbf.backend.utils.Result;
+import okhttp3.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.io.IOException;
 import java.util.Map;
-
-import com.sepbf.backend.pojo.domain.toFlask;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/stream")
 public class StreamController {
+    public static final OkHttpClient HTTP_CLIENT = new OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build();
 
     @Resource
     private ActionService actionService;
 
     @PostMapping("/limitTime")
-    public toFlask deliverToFlask (@org.springframework.web.bind.annotation.RequestBody Map<String,Object> map) throws IOException {
+    public Result deliverToFlask (@org.springframework.web.bind.annotation.RequestBody Map<String,Object> map) throws IOException {
         System.out.println(map);
         Integer streamCode =Integer.parseInt( String.valueOf(map.get("streamCode")));
         String actionName = String.valueOf(map.get("actionName"));
@@ -34,6 +41,14 @@ public class StreamController {
         toFlask flask = new toFlask(streamCode, actionName, limitTime);
 //        Request request = new Request.Builder()
 //                .url("http://
-    return flask;
+        Request request = new Request.Builder()
+                .url("http://127.0.0.1:5000/exercise")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+        Response response = HTTP_CLIENT.newCall(request).execute();
+        String responseBody = response.body().string();
+        System.out.println(responseBody);
+    return Result.success(responseBody);
     }
 }
